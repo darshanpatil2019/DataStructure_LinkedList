62a49ba4d71481ad085e232063da8aec80dc26f5
Darshan Patil <iampatildarsh>
2023-01-05 15:08:06 +0530

    Adding first Node 70, then 30, then 56 in LinkedList

--- Day14_DataStructures/src/MyLinkedList.java
+++ Day14_DataStructures/src/MyLinkedList.java
@@ -7,6 +7,8 @@ public class MyLinkedList {
         linkedList.insertFirst(70);  // insert 70 in LinkedList
         linkedList.insertFirst(30);  // insert 50 in LinkedList
         linkedList.insertFirst(56);  // insert 36 in LinkedList
+
+        linkedList.showLinkedList();  //showing added data in Linked List
     }
 }
 
--- Day14_DataStructures/src/Node.java
+++ Day14_DataStructures/src/Node.java
@@ -1,6 +1,12 @@
+/* UC2- Ability to create Linked List by adding 30 and 56 to 70
+        - Node with data 70 is First Created
+        - Next 30 is added to 70
+        - Finally 56 is added to 30
+        - LinkedList Sequence: 56->30->70*/
+
 public class Node {
-    private int data;
-    private Node next;
+    public int data;
+    public Node next;
 
     //created a Node constructor for creating a new Node
     Node(int data) {
@@ -16,5 +22,26 @@ class LinkedList {
     // now inserting data in new Node
     public void insertFirst(int data) {
         Node newNode = new Node(data);
+        //Adding Data in Node
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+    }
+
+    //Display Node in Linked List
+    public void showLinkedList() {
+        if (head == null) {
+            System.out.println("Linked List is Empty.");
+        } else {
+            Node temp = head;
+            while (temp != null) {
+                System.out.print(temp.data + " -> ");
+                temp = temp.next;
+            }
+        }
     }
 }
